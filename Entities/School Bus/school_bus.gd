e9117313d2d0940d8812dcce72c99eaffc685164
Ryan Beard <massive.cake09@gmail.com>
2025-08-16 13:12:53 -0700

    Fixed rollback of vehicle when using brake
    Added parking brake.
    Moved input out of physics process function and into the input function godot uses.

--- Entities/School Bus/school_bus.gd
+++ Entities/School Bus/school_bus.gd
@@ -2,54 +2,95 @@ extends CharacterBody2D
 
 
 var wheel_base:float = 70  # Distance from front to rear wheel
-var steering_angle:float = 15  # Amount that front wheel turns, in degrees
+var steering_angle:float = 20  # Amount that front wheel turns, in degrees
 
 var engine_power:float = 900  # Forward acceleration force.
+var service_brake_power:float = -450
+var parking_brake_power:float = -1500
 
 var friction:float = -55
 var drag:float = -0.06
 
-var braking:float = -450
-var max_speed_reverse:float = 250
 
-
-var acceleration:Vector2 = Vector2.ZERO
-
-
+var acceleration:Vector2
+var engine:float
+var braking:float
+var is_service_brake:bool
+var is_parking_brake:bool
 var steer_direction:float
 
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("steer_left") or event.is_action_pressed('steer_right'):
+		var turn:float = Input.get_axis("steer_left", "steer_right")
+		steer_direction = turn * deg_to_rad(steering_angle)
+		return
+	
+	if event.is_action_released("steer_left") or event.is_action_released('steer_right'):
+		steer_direction = 0.0
+		return
+	
+	if event.is_action_pressed('accelerate'):
+		engine = engine_power
+		return
+	
+	if event.is_action_released('accelerate'):
+		engine = 0.0
+		return
+	
+	if event.is_action_pressed("service_brake"):
+		is_service_brake = true
+		return
+	
+	if event.is_action_released('service_brake'):
+		is_service_brake = false
+		return
+	
+	if event.is_action_pressed('toggle_parking_brake'):
+		is_parking_brake = !is_parking_brake
+
 func _physics_process(delta: float) -> void:
-	acceleration = Vector2.ZERO
-	get_input()
+	calculate_acceleration()
 	apply_friction(delta)
 	calculate_steering(delta)
 	velocity += acceleration * delta
+	
+	# Fix service brake from rolling the vehicle back
+	if (is_service_brake or is_parking_brake) and velocity.length() < 50:
+		velocity = Vector2.ZERO
+	
 	move_and_slide()
 
-func get_input() ->void :
-	var turn = Input.get_axis("steer_left", "steer_right")
-	steer_direction = turn * deg_to_rad(steering_angle)
-	if Input.is_action_pressed("accelerate"):
-		acceleration = transform.x * engine_power
-	if Input.is_action_pressed("brake"):
-		acceleration = transform.x * braking
+func calculate_acceleration() -> void:
+	braking = 0
+	
+	if is_service_brake:
+		braking += service_brake_power
+	
+	if is_parking_brake:
+		braking += parking_brake_power
+	
+	acceleration = transform.x * (braking + engine)
 
 func apply_friction(delta: float) -> void:
-	if acceleration == Vector2.ZERO and velocity.length() < 50:
-		velocity = Vector2.ZERO
-	var friction_force = velocity * friction * delta
-	var drag_force = velocity * velocity.length() * drag * delta
+	# Friction is the force applied by the ground and is proportional to velocity
+	var friction_force:Vector2 = velocity * friction * delta
+	# Drag force is the force applied by wind resistance and is proportinal to velocity squared
+	var drag_force:Vector2 = velocity * velocity.length() * drag * delta
+	
 	acceleration += drag_force + friction_force
 
 func calculate_steering(delta: float) -> void:
-	# 1. Find the wheel positions
-	var rear_wheel = position - transform.x * wheel_base / 2.0
-	var front_wheel = position + transform.x * wheel_base / 2.0
-	# 2. Move the wheels forward
+	# Find the wheel positions
+	var rear_wheel:Vector2 = position - transform.x * wheel_base / 2.0
+	var front_wheel:Vector2 = position + transform.x * wheel_base / 2.0
+	
+	# Move the wheels forward
 	rear_wheel += velocity * delta
 	front_wheel += velocity.rotated(steer_direction) * delta
-	# 3. Find the new direction vector
-	var new_heading = rear_wheel.direction_to(front_wheel)
-	# 4. Set the velocity and rotation to the new direction
+	
+	# Find the new direction vector
+	var new_heading:Vector2 = rear_wheel.direction_to(front_wheel)
+	
+	# Set the velocity and rotation to the new direction
 	velocity = new_heading * velocity.length()
 	rotation = new_heading.angle()

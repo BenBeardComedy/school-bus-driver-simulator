4f0cbb2f54942095044da529d9ee67bd556a0e9e
Ryan Beard <massive.cake09@gmail.com>
2025-08-17 09:29:19 -0700

    Adding gear shifting
    Added reverse and neutral gears to the school bus

--- Entities/School Bus/school_bus.gd
+++ Entities/School Bus/school_bus.gd
@@ -1,22 +1,31 @@
 extends CharacterBody2D
 
 
+enum Gear {
+	REVERSE,
+	NEUTRAL,
+	DRIVE
+}
+
+
 var wheel_base:float = 70  # Distance from front to rear wheel
 var steering_angle:float = 20  # Amount that front wheel turns, in degrees
 
 var engine_power:float = 900  # Forward acceleration force.
+var reverse_engine_power:float = -400
 var service_brake_power:float = -450
 var parking_brake_power:float = -1500
 
-var friction:float = -55
-var drag:float = -0.06
+var friction:float = -55 # Ground friction
+var drag:float = -0.06 # Wind friction
 
+var current_gear:Gear = Gear.NEUTRAL
 
 var acceleration:Vector2
 var engine:float
 var braking:float
 var is_service_brake:bool
-var is_parking_brake:bool
+var is_parking_brake:bool = true
 var steer_direction:float
 
 func _unhandled_input(event: InputEvent) -> void:
@@ -30,7 +39,10 @@ func _unhandled_input(event: InputEvent) -> void:
 		return
 	
 	if event.is_action_pressed('accelerate'):
-		engine = engine_power
+		if current_gear == Gear.DRIVE:
+			engine = engine_power
+		elif current_gear == Gear.REVERSE:
+			engine = reverse_engine_power
 		return
 	
 	if event.is_action_released('accelerate'):
@@ -47,11 +59,22 @@ func _unhandled_input(event: InputEvent) -> void:
 	
 	if event.is_action_pressed('toggle_parking_brake'):
 		is_parking_brake = !is_parking_brake
+	
+	if event.is_action_pressed('shift_gear_up'):
+		var next_gear:int = current_gear + 1
+		if next_gear < Gear.size():
+			current_gear = next_gear
+	
+	if event.is_action_pressed('shift_gear_down'):
+		var next_gear:int = current_gear - 1
+		if next_gear >= 0:
+			current_gear = next_gear
 
 func _physics_process(delta: float) -> void:
 	calculate_acceleration()
 	apply_friction(delta)
 	calculate_steering(delta)
+	
 	velocity += acceleration * delta
 	
 	# Fix service brake from rolling the vehicle back
@@ -61,13 +84,18 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 
 func calculate_acceleration() -> void:
-	braking = 0
+	# Reset and recalculate the brake power
+	braking = 0.0
+	
+	var brake_modifier := 1.0
+	if current_gear == Gear.REVERSE:
+		brake_modifier = -1.0
 	
 	if is_service_brake:
-		braking += service_brake_power
+		braking += service_brake_power * brake_modifier
 	
 	if is_parking_brake:
-		braking += parking_brake_power
+		braking += parking_brake_power * brake_modifier
 	
 	acceleration = transform.x * (braking + engine)
 
@@ -89,8 +117,12 @@ func calculate_steering(delta: float) -> void:
 	front_wheel += velocity.rotated(steer_direction) * delta
 	
 	# Find the new direction vector
-	var new_heading:Vector2 = rear_wheel.direction_to(front_wheel)
+	var new_heading:Vector2 = (front_wheel - rear_wheel).normalized()
+	var d:float = new_heading.dot(velocity.normalized())
 	
 	# Set the velocity and rotation to the new direction
-	velocity = new_heading * velocity.length()
+	if d > 0:
+		velocity = new_heading * velocity.length()
+	if d < 0:
+		velocity = -new_heading * velocity.length()
 	rotation = new_heading.angle()
